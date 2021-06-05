89109e66c25d471aeee51035be9f5184a75247ee
Daniel Eichhorn <dani.eichhorn@squix.ch>
2021-06-05 11:06:28 +0200

    Initial import

---
+++ src/settings.h
@@ -0,0 +1,19 @@
+#define WIFI_SSID "yourssid"
+#define WIFI_PASSWORD "yourpassw0rd"
+
+#define MQTT_HOST IPAddress(192, 168, 0, 100)
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "ESPNOW/1"
+
+#define GATEWAY_RXD2 14
+#define GATEWAY_TXD2 15
+#define BAUDRATE 9600
+
+#define ESP1_LED_PIN 32
+#define ESP2_LED_PIN 32
+#define LED_COUNT   2
+#define CHANNEL     0
+
+
+
+
